70c8bbf3289b58909565dfa869817d99b72dbc35
akshatnehra <akshatnehra1999@gmail.com>
2022-02-20 07:33:13 +0530

    v1 simple hex

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var colors = ["#ff0000","#918563","#aaff1c","#eaccff","#006497","#aaccaa","#f0e6fa"];
+
+var bg = document.querySelector("section");
+var btn = document.getElementById("butn");
+var text = document.getElementById("text");
+
+btn.addEventListener("click", function(){
+    let randomNumber = parseInt(Math.random()*colors.length);
+    console.log("Hey");
+    bg.style.background = colors[randomNumber];
+    // console.log(colors[randomNumber]);
+    // console.log(randomNumber);
+    text.textContent=(colors[randomNumber]);
+});
